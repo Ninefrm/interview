28b481cd7a1e3320b7b0299dc14488a891de52dd
Maximiliano Fonseca <maxfonseca.r@gmail.com>
2022-04-01 01:02:01 -0600

    2/3 test

--- profile_card.js
+++ profile_card.js
@@ -155,75 +155,76 @@ body {
 `
 
 
-function createCard(profileData){
+function createCard(profileData) {
     var cardTemplate = `<html>` +
-    cardStyle + 
-    `<div class="border-card">` +
-    `<div class="row bottom-border">` +
-    `<div class="column left">` +
-    `<div class="circle-div">` +
-    `<img class="profile-picture" id="profile-picture" src="${profileData.picture}" alt="Profile picture">` +
-    `</div>` +
-    `</div>` +
-    `<div class="column right">` +
-    `<div class="profile-name">` +
-    `<span id="profile-name">${profileData.name}</span>` +
-    `<div class="profile-carrer">` +
-    `<span id="profile-carrer">${profileData.carrer}</span>` +
-    `</div>` +
-    `</div>` +
-    `</div>` +
-    `</div>` +
-    `<div class="row">` +
+        cardStyle +
+        `<div class="border-card">` +
+        `<div class="row bottom-border">` +
+        `<div class="column left">` +
+        `<div class="circle-div">` +
+        `<img class="profile-picture" id="profile-picture" src="${profileData.picture}" alt="Profile picture">` +
+        `</div>` +
+        `</div>` +
+        `<div class="column right">` +
+        `<div class="profile-name">` +
+        `<span id="profile-name">${profileData.name}</span>` +
+        `<div class="profile-carrer">` +
+        `<span id="profile-carrer">${profileData.carrer}</span>` +
+        `</div>` +
+        `</div>` +
+        `</div>` +
+        `</div>` +
+        `<div class="row">` +
 
-    `<div class="two-col">` +
-    `<p class="low-title">PROFILE</p>` +
-    `<p class="justify-text" id="profile-description">${profileData.profile_description}</p>` +
-    `</div>` +
-    `<div class="two-col">` +
-    `<p class="low-title center-text">CONTACT</p>` +
-    `<div class="row">` +
-    `<div class="three-col">` +
-    `<div class="center-text">` +
-    `<i class="middle-icon material-icons">email</i>` +
-    `<p class="middle-icon-text">` +
-    `<a target="_blank"href="mailto:${profileData.email_user}?subject=Card contact" id="profile-email">${profileData.email_user}</a></p>` +
-    `</div>` +
-    `</div>` +
-    `<div class="three-col">` +
-    `<div class="center-text">` +
-    `<i class="middle-icon material-icons">local_phone</i>` +
-    `<p class="middle-icon-text"><a target="_blank"href="https://wa.me/${profileData.phone_user}" id="profile-phone">${profileData.phone_user}</a></p>` +
-    `</div>` +
-    `</div>` +
-    `<div class="three-col">` +
-    `<div class="center-text">` +
-    `<a target="_blank"id="profile-linkedin" href="https://www.linkedin.com/in/${profileData.username}">` +
-    `<svg viewBox="0 0 512 512" height="6.8vw">` +
-    `<path fill="" d="M150.65,100.682c0,27.992-22.508,50.683-50.273,50.683c-27.765,0-50.273-22.691-50.273-50.683 C50.104,72.691,72.612,50,100.377,50C128.143,50,150.65,72.691,150.65,100.682z M143.294,187.333H58.277V462h85.017V187.333z M279.195,187.333h-81.541V462h81.541c0,0,0-101.877,0-144.181c0-38.624,17.779-61.615,51.807-61.615 c31.268,0,46.289,22.071,46.289,61.615c0,39.545,0,144.181,0,144.181h84.605c0,0,0-100.344,0-173.915 s-41.689-109.131-99.934-109.131s-82.768,45.369-82.768,45.369V187.333z" />` +
-    `</svg>` +
-    `<p class="middle-icon-text" id="profile-username_user">${profileData.username}</p>` +
-    `</a>` +
-    `</div>` +
-    `</div>` +
-    `</div>` +
-    `</div>` +
-    `</div>` +
-    `</div>` +
-    `</html>`;
+        `<div class="two-col">` +
+        `<p class="low-title">PROFILE</p>` +
+        `<p class="justify-text" id="profile-description">${profileData.profile_description}</p>` +
+        `</div>` +
+        `<div class="two-col">` +
+        `<p class="low-title center-text">CONTACT</p>` +
+        `<div class="row">` +
+        `<div class="three-col">` +
+        `<div class="center-text">` +
+        `<i class="middle-icon material-icons">email</i>` +
+        `<p class="middle-icon-text">` +
+        `<a target="_blank"href="mailto:${profileData.email_user}?subject=Card contact" id="profile-email">${profileData.email_user}</a></p>` +
+        `</div>` +
+        `</div>` +
+        `<div class="three-col">` +
+        `<div class="center-text">` +
+        `<i class="middle-icon material-icons">local_phone</i>` +
+        `<p class="middle-icon-text"><a target="_blank"href="https://wa.me/${profileData.phone_user}" id="profile-phone">${profileData.phone_user}</a></p>` +
+        `</div>` +
+        `</div>` +
+        `<div class="three-col">` +
+        `<div class="center-text">` +
+        `<a target="_blank"id="profile-linkedin" href="https://www.linkedin.com/in/${profileData.username}">` +
+        `<svg viewBox="0 0 512 512" height="6.8vw">` +
+        `<path fill="" d="M150.65,100.682c0,27.992-22.508,50.683-50.273,50.683c-27.765,0-50.273-22.691-50.273-50.683 C50.104,72.691,72.612,50,100.377,50C128.143,50,150.65,72.691,150.65,100.682z M143.294,187.333H58.277V462h85.017V187.333z M279.195,187.333h-81.541V462h81.541c0,0,0-101.877,0-144.181c0-38.624,17.779-61.615,51.807-61.615 c31.268,0,46.289,22.071,46.289,61.615c0,39.545,0,144.181,0,144.181h84.605c0,0,0-100.344,0-173.915 s-41.689-109.131-99.934-109.131s-82.768,45.369-82.768,45.369V187.333z" />` +
+        `</svg>` +
+        `<p class="middle-icon-text" id="profile-username_user">${profileData.username}</p>` +
+        `</a>` +
+        `</div>` +
+        `</div>` +
+        `</div>` +
+        `</div>` +
+        `</div>` +
+        `</div>` +
+        `</html>`;
     return cardTemplate;
 }
 function profileCard(profileData) {
     const fs = require('fs')
     var result = "";
     if (profileData?.name != "" && profileData?.carrer != "" && profileData?.description != "" && profileData?.email_user != "" && profileData?.phone_user != "" && profileData?.username != "") {
-        fs.writeFile('./'+profileData.name+'_card.html', createCard(profileData), err => {
+        fs.writeFile('./' + profileData.name + '_card.html', createCard(profileData), err => {
             if (err) {
-              console.error(err)
-              return false;
+                console.error(err)
+                return false;
             }
             return true;
-          })
+        })
+        return true
     }
     return false;
 }
--- test/profile_card.test.js
+++ test/profile_card.test.js
@@ -23,13 +23,28 @@ describe("Tests", () => {
             true);
     });
     it("more data", () => {
-
-        assert.strictEqual(profile_card(),
+        var profileDataNew2 = {
+            name: "Dr. Jerel Pric",
+            carrer: "Murray, Runolfsdottir and Murazik",
+            profile_description: "Brown, shoulder-length hair is pulled back to reveal a fresh, lived-in face. Shining green eyes, set narrowly within their sockets, watch anxiously over the children they've grown affactionate of for so long. Fair skin gracefully compliments his hair and leaves a bittersweet memory of his luck. This is the face of Almon Moonwalker, a true globetrotter among high elves. He stands towering above others, despite his athletic frame. There's something seductive about him, perhaps it's his sense of comradery or perhaps it's simply his persistence. But nonetheless, people tend to buy him a drink, while secretly training to become more like him.",
+            picture: "https://picsum.photos/200/300?random="+Math.random(), //Preventing creating a file upload function 
+            email_user: "cindy.flatley@mphaotu.com",
+            phone_user: "4929637710831685",
+            username: "rafaelamurphy",
+            cellphone: "46212351231",
+            blood_type: "O-"
+        };
+        assert.strictEqual(profile_card(profileDataNew2),
             true);
     });
     it("Less data", () => {
-
-        assert.strictEqual(profile_card(),
+        var profileDataNew2 = {
+            name: "Torey Balistreri",
+            carrer: "Murray, Runolfsdottir and Murazik",
+            profile_description: "Brown, shoulder-length hair is pulled back to reveal a fresh, lived-in face. Shining green eyes, set narrowly within their sockets, watch anxiously over the children they've grown affactionate of for so long. Fair skin gracefully compliments his hair and leaves a bittersweet memory of his luck. This is the face of Almon Moonwalker, a true globetrotter among high elves. He stands towering above others, despite his athletic frame. There's something seductive about him, perhaps it's his sense of comradery or perhaps it's simply his persistence. But nonetheless, people tend to buy him a drink, while secretly training to become more like him.",
+            picture: "https://picsum.photos/200/300?random="+Math.random() //Preventing creating a file upload function 
+        };
+        assert.strictEqual(profile_card(profileDataNew2),
             false);
     });
 });
