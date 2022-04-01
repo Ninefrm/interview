7a9cd6d05091afe705f5554d491b867008c84e0f
Maximiliano Fonseca <maxfonseca.r@gmail.com>
2022-03-31 22:31:04 -0600

    cardStyle variable to prevent losing the design

--- js/index.js
+++ js/index.js
@@ -8,7 +8,148 @@ var profileData = {
     username: "Ninefrm",
 };
 
-const cardTemplate = `<div class="border-card">` +
+const cardStyle = `
+<head>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<style>
+body {
+    font-family: 'Roboto', sans-serif;
+    font-size: 1vw;
+}
+
+.column {
+    float: left;
+}
+
+.left {
+    width: 27%;
+}
+
+.right {
+    width: 73%;
+}
+
+/* Clear floats after the columns */
+.row:after {
+    content: "";
+    display: table;
+    clear: both;
+}
+
+.two-col {
+    float: left;
+    width: 49%;
+    height: 30vh;
+    padding-left: 1vh;
+}
+
+.three-col {
+    float: left;
+    width: 31.3%;
+    height: 30vh;
+    padding-left: 1vh;
+}
+
+.justify-text {
+    text-align: justify;
+}
+
+.center-text {
+    text-align: center;
+}
+
+.bottom-border {
+    padding: 3vh;
+    border-bottom: 5px solid;
+}
+
+.inline-icon {
+    vertical-align: bottom;
+    font-size: 2vw !important;
+}
+
+.circle-div {
+    position: relative;
+    width: 90%;
+    padding-bottom: 90%;
+    border-radius: 50%;
+    overflow: hidden;
+}
+
+.profile-name {
+    padding: 3vh 0;
+    text-align: left;
+    font-size: 5vw;
+    font-weight: bold;
+}
+
+.profile-name-carrer {
+    text-align: left;
+    font-size: 2vw;
+    font-weight: lighter;
+}
+
+.low-title {
+    font-size: 2vw;
+    font-weight: bold;
+}
+
+.profile-picture {
+    position: absolute;
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+}
+
+.input-container {
+    display: -ms-flexbox;
+    /* IE10 */
+    display: flex;
+    width: 100%;
+    margin-bottom: 5vh;
+}
+
+.icon {
+    padding: 3vw;
+    background: grey;
+    color: white;
+    min-width: 3vw;
+    text-align: center;
+}
+
+.input-field {
+    width: 100%;
+    padding: 3vw;
+    outline: none;
+}
+
+.no-resize {
+    resize: none;
+}
+
+.middle-icon {
+    vertical-align: bottom;
+    font-size: 7vw !important;
+}
+
+.middle-icon-text {
+    font-size: 1vw;
+    font-weight: bold;
+}
+
+.border-card {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    transition: 0.3s;
+}
+</style>
+`
+
+const cardTemplate = cardStyle + 
+    `<div class="border-card">` +
     `<div class="row bottom-border">` +
     `<div class="column left">` +
     `<div class="circle-div">` +
@@ -60,6 +201,8 @@ const cardTemplate = `<div class="border-card">` +
     `</div>` +
     `</div>` +
     `</div>`;
+
+
 class profileCard extends HTMLElement {
     connectedCallback() {
         this.innerHTML = cardTemplate;
